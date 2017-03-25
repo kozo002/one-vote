191058fe65a6e1ffb778b5ffc6ccd0cf7ddd8c3a
kozo yamagata <tune002@gmail.com>
2017-03-25 18:59:45 +0900

    Add helper tests

---
+++ tests/unit/helpers/choice-example-test.js
@@ -0,0 +1,12 @@
+
+import { choiceExample } from 'one-vote/helpers/choice-example';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | choice example');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = choiceExample([42]);
+  assert.ok(result);
+});
+
