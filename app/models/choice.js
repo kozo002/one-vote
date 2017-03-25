1a7905a13c58052bcbd452822d997aae3e8098d8
kozo yamagata <tune002@gmail.com>
2017-03-25 23:54:48 +0900

    Add vote methods to choice model

--- app/models/choice.js
+++ app/models/choice.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 const {
   attr,
@@ -10,4 +11,28 @@ export default DS.Model.extend({
   body: attr("string"),
   questionary: belongsTo("questionary"),
   votes: hasMany("vote"),
+
+  votesCount: Ember.computed("votes.@each", function() {
+    return this.get("votes.length");
+  }),
+
+  isVotedBy(guestKey) {
+    const votes = this.get("votes");
+    const vote = votes.find((vote) => vote.get("guestKey") === guestKey);
+    return !Ember.isEmpty(vote);
+  },
+
+  addVoteBy(guestKey) {
+    if (this.isVotedBy(guestKey)) { return; }
+
+    const store = this.get("store");
+    const votes = this.get("votes");
+    votes.pushObject(store.createRecord("vote", { guestKey }));
+  },
+
+  loseVoteBy(guestKey) {
+    this.set("votes", this.get("votes").filter((vote) => {
+      return vote.get("guestKey") !== guestKey;
+    }));
+  }
 });
