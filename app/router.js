99868b1f5cadf81f0adfa88d1348288ea0375ed6
kozo yamagata <tune002@gmail.com>
2017-03-25 22:02:05 +0900

    Make questionary page

--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route("home", { path: "/" })
+  this.route("home", { path: "/" });
+  this.route("questionary", { path: "q/:questionaryId" });
 });
 
 export default Router;
