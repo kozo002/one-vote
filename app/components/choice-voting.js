396774613a402462207206d0b2bdb2559e2ea179
kozo yamagata <tune002@gmail.com>
2017-03-26 15:13:43 +0900

    Add disabled attribute to choice

--- app/components/choice-voting.js
+++ app/components/choice-voting.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isDisabled: Ember.computed("isVoted", "isAnyVoted", function() {
+    const isAnyVoted = this.get("isAnyVoted");
+    const isVoted = this.get("isVoted");
+    return isAnyVoted && !isVoted;
+  }),
+
   actions: {
     vote() {
       this.sendAction("onVote", this.get("choice"));
