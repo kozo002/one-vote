92bee8e2b6d9fbdd6893e547308ff910474a2577
kozo yamagata <tune002@gmail.com>
2017-03-30 18:18:23 +0900

    Install emberfire

--- config/environment.js
+++ config/environment.js
@@ -17,6 +17,14 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
