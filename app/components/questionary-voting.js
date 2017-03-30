30b87f766e7e1af99e5aba221775e65667b5ff50
kozo yamagata <tune002@gmail.com>
2017-03-30 18:25:22 +0900

    Update questionary-voting component

--- app/components/questionary-voting.js
+++ app/components/questionary-voting.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
-import uuidV4 from 'npm:uuid/v4';
 
 export default Ember.Component.extend({
-  votedIndex: Ember.computed("questionary.choices.@each.votesCount", function() {
+  votedIndex: Ember.computed("questionary.choices.@each.votes", function() {
     const choices = this.get("questionary.choices");
     const guestKey = this.guestKey.get("value");
     let result;
